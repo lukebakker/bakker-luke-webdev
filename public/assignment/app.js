edf9f7bdc48821a2bf84122fa5281194ce8eb6ba
Luke Bakker <bakker.l@husky.neu.edu>
2017-07-21 12:09:12 -0400

    commit

--- public/assignment/app.js
+++ public/assignment/app.js
@@ -1,28 +1,10 @@
 /**
  * Created by lbakker on 7/17/17.
  */
-var app = angular.module("WamApp", [])
 
-app.controller("loginController", loginController);
-// JSON = JavaScript Object Notation
-var users = [
-    {_id: "123", username: "alice", password: "alice", firstName: "Alice", lastName: "Wonder"},
-    {_id: "234", username: "bob", password: "bob", firstName: "Bob", lastName: "Marley"},
-    {_id: "345", username: "charly", password: "charly", firstName: "Charly", lastName: "Garcia"},
-    {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose", lastName: "Annunzi"}
-]
+(function () {
 
-function loginController($scope) {
+    angular.module("WamApp", ["ngRoute"]);
 
-    $scope.login = function (users) {
-        for (var u in users) {
-            var _user = users[u];
-            if (_user.username === user.username && _user.password === user.password) {
-                $scope.welcomeUser = _user;
-            }
-        }
-        /*$scope.login = function (user) {
-         alert(user.username + ' ' + user.password);
-         }*/
-    }
-}
+})();
+
