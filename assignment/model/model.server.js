ad0ee8cff7163859561fdcc747dc4796fef1df67
Luke Bakker <bakker.l@husky.neu.edu>
2017-08-09 18:06:11 -0400

    trying to get heroku back up

--- assignment/model/model.server.js
+++ assignment/model/model.server.js
@@ -1,12 +1,12 @@
 var q = require('q');
 
 var connectionString = 'mongodb://127.0.0.1:27017/webdev_summer2_2017'; // for local
-/*if (process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
+if (process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
     var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
     var password = process.env.MLAB_PASSWORD_WEBDEV;
     connectionString = 'mongodb://' + username + ':' + password;
     connectionString += '@ds137220.mlab.com:37220/heroku_xkpv7xx0'; // user yours
-}*/
+}
 // Replace "@ds157268.mlab.com:57268/heroku_nh37fqq4"
 // above with your own URL given to you by mLab
 
