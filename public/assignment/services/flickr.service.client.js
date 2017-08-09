3b43f83b973910b87cf0e61f3e32b63a26f4f942
Luke Bakker <bakker.l@husky.neu.edu>
2017-08-08 22:22:24 -0400

    finished flickr

--- public/assignment/services/flickr.service.client.js
+++ public/assignment/services/flickr.service.client.js
@@ -1,31 +1,25 @@
 (function () {
     angular
         .module("WamApp")
-        .service("flickrService", flickrService);
+        .factory("flickrService", flickrService);
 
-    function flickrService($http) {
+    function flickrService($http, widgetService) {
 
         var api = {
             "searchPhotos": searchPhotos,
-            "selectPhoto": selectPhoto
         };
 
-        var key = "d0ff7694b51bb4e0290a840fc5ed6341";
-        var secret = "6a108e67ed6bfd59";
-        var urlBase = "https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&api_key=API_KEY&text=TEXT";
+        return api;
 
         function searchPhotos(searchTerm) {
+            var urlBase = "https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&api_key=API_KEY&text=TEXT"
+            var key = "d0ff7694b51bb4e0290a840fc5ed6341";
+            var secret = "6a108e67ed6bfd59";
+
+
             var url = urlBase.replace("API_KEY", key).replace("TEXT", searchTerm);
             return $http.get(url);
         }
 
-        function selectPhoto(photo, websiteId, pageId, widgetId) {
-            var url = "https://farm" + photo.farm + ".staticflickr.com/" + photo.server;
-            url += "/" + photo.id + "_" + photo.secret + "_b.jpg";
-            WidgetService
-                .updateWidget(websiteId, pageId, widgetId, {url: url})
-                .then()
-            ;
-        }
     }
 })();
--- public/assignment/views/widget/controllers/flickrImageSearch.controller.js
+++ public/assignment/views/widget/controllers/flickrImageSearch.controller.js
@@ -3,12 +3,14 @@
         .module("WamApp")
         .controller("flickrImageSearchController", flickrImageSearchController);
 
-    function flickrImageSearchController($routeParams, widgetService, flickrService) {
+    function flickrImageSearchController($routeParams, widgetService, flickrService, $location) {
         var model = this;
         model.userId = $routeParams.userId;
         model.pageId = $routeParams.pageId;
         model.websiteId = $routeParams.websiteId;
         model.widgetId = $routeParams.widgetId;
+        model.photos = [];
+        model.selectPhoto = selectPhoto;
 
 
         function init() {
@@ -17,14 +19,12 @@
                 .then(function (widget) {
                     model.widget = widget;
                 });
-            console.log("here");
         }
 
         init();
 
 
         model.searchPhotos = function (searchTerm) {
-            console.log(searchTerm);
             flickrService
                 .searchPhotos(searchTerm)
                 .then(function (response) {
@@ -33,9 +33,29 @@
                     data = JSON.parse(data);
                     model.photos = data.photos;
                 });
+        };
+
+        function selectPhoto(photo) {
+            var key = "d0ff7694b51bb4e0290a840fc5ed6341";
+            var secret = "6a108e67ed6bfd59";
+
+            var url = "https://farm" + photo.farm + ".staticflickr.com/" + photo.server;
+            url += "/" + photo.id + "_" + photo.secret + "_b.jpg";
+
+            var widget = {
+                "_id": model.widgetId, "type": 'IMAGE', "pageId": model.pageId,
+                "width": 100, "size": 0, "text": "", "url": url
+            };
+            widgetService
+                .updateWidget(model.userId, model.websiteId, model.pageId, model.widgetId, widget)
+                .then(function (update) {
+                    $location.url("/user/" + model.userId + "/website/" + model.websiteId + "/page/" + model.pageId +
+                        "/widget");
+                });
         }
+
+
     }
 
 
-})
-();
+})();
